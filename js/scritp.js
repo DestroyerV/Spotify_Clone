e137ba3244bbb0fe3c899c423f5bf610ca4353c8
Vaibhav Mishra <95367218+DestroyerV@users.noreply.github.com>
2024-02-18 13:05:49 +0530

    fix the bug where playlists was not showing

--- js/scritp.js
+++ js/scritp.js
@@ -82,8 +82,8 @@ async function displayAlbums() {
   let array = Array.from(anchors);
   for (let index = 0; index < array.length; index++) {
     const e = array[index];
-    if (e.href.includes("songs") && !e.href.includes(".htaccess")) {
-      let folder = e.href.split("/").slice(-2)[0];
+    if (e.href.includes("songs")) {
+      let folder = e.href.split("/").slice(-1)[0];
 
       // Get the metadata of the folder
       let a = await fetch(`https://spotify-clone-seven-sage.vercel.app/songs/${folder}/info.json`);
